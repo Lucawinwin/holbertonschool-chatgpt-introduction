68c1640b54484d096a5db3ecf69fde6ac8e8724c
Lucawinwin <windwehrluca@gmail.com>
2025-01-15 09:26:19 +0100

    test

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,6 +1,20 @@
-def factorial(n):
-    result = 1
+#!/usr/bin/python3
+import sys
+
+def factorielle(n):
+    resultat = 1
     while n > 1:
-        result *= n
-        n -= 1  # Décrémenter n à chaque itération
-    return result
+        resultat *= n
+        n -= 1  # Décrémenter n pour éviter une boucle infinie
+    return resultat
+
+# Vérifier qu'un argument a été passé
+if len(sys.argv) != 2:
+    print("Veuillez fournir un nombre en argument.")
+    sys.exit(1)
+
+# Calculer et afficher la factorielle
+n = int(sys.argv[1])
+f = factorielle(n)
+print(f)
+
